Bugfix: add softmax

g_invariance/model.py
@@ -5,12 +5,37 @@ various pooling approaches.
 import torch
 from escnn import gspaces
 from torch import nn
-
+import torch.nn.functional as F
 import g_invariance.modules as gtc_modules
 import g_invariance.pooling as gtc_pooling
 
+class VanillaNet(nn.Module):
+    def __init__(self, config):
+        super(VanillaNet, self).__init__()
 
-class Net(nn.Module):
+        layer_1_size = config.fc_sizes[0]
+        layer_2_size = config.fc_sizes[1]
+
+        # mnist images are (1, 28, 28) (channels, width, height)
+        self.layer_1 = torch.nn.Linear(16 * 16, layer_1_size)
+        self.layer_2 = torch.nn.Linear(layer_1_size, layer_2_size)
+        self.layer_3 = torch.nn.Linear(layer_2_size, config.fc_sizes[3])
+
+    def forward(self, x):
+        batch_size, channels, width, height = x.size()
+        x = x.view(batch_size, -1)
+
+        x = self.layer_1(x)
+        x = F.relu(x)
+
+        x = self.layer_2(x)
+        x = F.relu(x)
+
+        x = self.layer_3(x)
+        return F.log_softmax(x, dim=1)
+
+
+class GInvNet(nn.Module):
     # TODO: Currently the output size is hardcoded, should be
     # computed from the input size, conv and the pooling layer
 
@@ -33,7 +58,7 @@ class Net(nn.Module):
             N=config.N,
             # Should this match SO2/O2? i.e no flip?
             action=gspaces.flipRot2dOnR2,
-            n_channels=config.n_channels,
+            n_channels=config.n_filters,
             kernel_size=16,
             padding=0,
             bias=False,
@@ -55,4 +80,4 @@ class Net(nn.Module):
         )
 
     def forward(self, x):
-        return self.model(x)
+        return F.log_softmax(self.model(x))

g_invariance/train.py
@@ -11,7 +11,6 @@ from filelock import FileLock
 from torch.utils.data import DataLoader, random_split
 from torchmetrics import Accuracy
 from torchvision import transforms as torchvision_transforms
-
 import g_invariance.dataset as g_dataset
 from g_invariance import model, rich_gi
 
@@ -43,7 +42,7 @@ class MNISTClassifier(pl.LightningModule):
         super(MNISTClassifier, self).__init__()
         self.accuracy = Accuracy(task='multiclass', num_classes=config.fc_sizes[-1], top_k=1)
         self.lr = config['lr']
-        self.model = model.Net(config)
+        self.model = getattr(model, config.model_name)(config)
         self.eval_loss = []
         self.eval_accuracy = []
         self.config = config
@@ -83,15 +82,7 @@ class MNISTClassifier(pl.LightningModule):
 
     def configure_optimizers(self):
         optimizer = torch.optim.Adam(self.parameters(), lr=self.lr)
-        lr_scheduler = torch.optim.lr_scheduler.CosineAnnealingWarmRestarts(
-            optimizer, T_0=1, T_mult=30, verbose=True
-        )
-
-        return {
-            'optimizer': optimizer,
-            'lr_scheduler': lr_scheduler,
-            'monitor': 'ptl/val_loss',
-        }
+        return optimizer
 
 
 class MNISTDataModule(pl.LightningDataModule):