3f43133b3c613b7e05a2f1849ae687aff6e3f734
Johan Mathe <johan.mathe@gmail.com>
2024-08-02 17:21:13 +0000

    Add config for ray search

--- mataigne_2024.py
+++ mataigne_2024.py
@@ -30,7 +30,7 @@ def train_func(config):
     config_param.update(config)
     setup_wandb(config)
     dm = g_train.MNISTDataModule(config_param)
-    model = g_train.MNISTClassifier(config_param)
+    model = g_train.Classifier(config_param)
 
     pl.seed_everything(config_param.seed)
     trainer = pl.Trainer(
@@ -48,11 +48,11 @@ def train_func(config):
 
 def search_pooling():
     scaling_config = ScalingConfig(
-        num_workers=1, use_gpu=True, resources_per_worker={'CPU': 3, 'GPU': 1}
+        num_workers=1, use_gpu=True, resources_per_worker={'CPU': 10, 'GPU': 1}
     )
 
     run_config = RunConfig(
-        callbacks=[WandbLoggerCallback(project='g-invariance-search-pooling')],
+        callbacks=[WandbLoggerCallback(project='g-invariance-search-cifar-pooling-lr')],
         checkpoint_config=CheckpointConfig(
             num_to_keep=1,
             checkpoint_score_attribute='ptl/val_accuracy',
@@ -70,14 +70,10 @@ def search_pooling():
     def spec_to_target_params(spec):
         fc_sizes = {
             'cyclic': {
-                'MNIST': 40000,
-                'EMNIST': 40000,
-                'CIFAR10': 500000,
+                'CIFAR10': 1000000,
             },
             'dihedral': {
-                'MNIST': 140000,
-                'EMNIST': 40000,
-                'CIFAR10': 500000,
+                'CIFAR10': 1000000,
             },
         }
         config = spec['train_loop_config']
@@ -90,31 +86,29 @@ def search_pooling():
 
     def n_filters(spec):
         n_filters_map = {
-            'cyclic': {'MNIST': 24, 'EMNIST': 24, 'CIFAR10': 32},
-            'dihedral': {'MNIST': 4, 'EMNIST': 20, 'CIFAR10': 32},
+            'cyclic': {'CIFAR10': 8},
+            'dihedral': {'CIFAR10': 8},
         }
         config = spec['train_loop_config']
         return n_filters_map[config['group']][config['dataset_name']]
 
     def fc_sizes(spec):
         fc_sizes_map = {
-            'MNIST': [64, 64, 10],
-            'EMNIST': [64, 64, 26],
             'CIFAR10': [128, 64, 10],
         }
         config = spec['train_loop_config']
         return fc_sizes_map[config['dataset_name']]
 
     def n_input_channels(spec):
-        n_input_channels_map = {'MNIST': 1, 'EMNIST': 1, 'CIFAR10': 3}
+        n_input_channels_map = { 'CIFAR10': 3}
         config = spec['train_loop_config']
         return n_input_channels_map[config['dataset_name']]
 
     search_space = {
         'pooling': tune.grid_search(['bsp', 'tc', 'max', 'avg']),
-        'dataset_name': tune.grid_search(['MNIST', 'EMNIST', 'CIFAR10']),
+        'dataset_name': tune.grid_search(['CIFAR10']),
         'group': tune.grid_search(['cyclic', 'dihedral']),
-        'img_size': tune.grid_search([16, 28, 45]),
+        'img_size': tune.grid_search([32]),
         'target_params_count': tune.sample_from(spec_to_target_params),
         'data_augmentation': tune.sample_from(data_augmentation),
         'fc_sizes': tune.sample_from(fc_sizes),
@@ -144,11 +138,11 @@ def search_pooling():
 
 def search_filters():
     scaling_config = ScalingConfig(
-        num_workers=1, use_gpu=True, resources_per_worker={'CPU': 3, 'GPU': 1}
+        num_workers=1, use_gpu=True, resources_per_worker={'CPU': 10, 'GPU': 1}
     )
 
     run_config = RunConfig(
-        callbacks=[WandbLoggerCallback(project='g-invariance-filters')],
+        callbacks=[WandbLoggerCallback(project='g-invariance-cifar10-search')],
         checkpoint_config=CheckpointConfig(
             num_to_keep=1,
             checkpoint_score_attribute='ptl/val_accuracy',
@@ -166,14 +160,10 @@ def search_filters():
     def spec_to_target_params(spec):
         fc_sizes = {
             'cyclic': {
-                'MNIST': 40000,
-                'EMNIST': 40000,
-                'CIFAR10': 500000,
+                'CIFAR10': 1000000,
             },
             'dihedral': {
-                'MNIST': 140000,
-                'EMNIST': 40000,
-                'CIFAR10': 500000,
+                'CIFAR10': 1000000,
             },
         }
         config = spec['train_loop_config']
@@ -184,31 +174,23 @@ def search_filters():
         config = spec['train_loop_config']
         return data_augmentation_map[config['group']]
 
-    def n_filters(spec):
-        n_filters_map = {
-            'cyclic': {'MNIST': 24},
-            'dihedral': {'MNIST': 4},
-        }
-        config = spec['train_loop_config']
-        return n_filters_map[config['group']][config['dataset_name']]
-
     def fc_sizes(spec):
         fc_sizes_map = {
-            'MNIST': [64, 64, 10],
+            'CIFAR10': [128, 64, 10],
         }
         config = spec['train_loop_config']
         return fc_sizes_map[config['dataset_name']]
 
     def n_input_channels(spec):
-        n_input_channels_map = {'MNIST': 1, 'EMNIST': 1, 'CIFAR10': 3}
+        n_input_channels_map = { 'CIFAR10': 3}
         config = spec['train_loop_config']
         return n_input_channels_map[config['dataset_name']]
 
     search_space = {
         'pooling': tune.grid_search(['bsp', 'tc', 'max', 'avg']),
-        'dataset_name': tune.grid_search(['MNIST']),
+        'dataset_name': tune.grid_search(['CIFAR10']),
         'group': tune.grid_search(['cyclic', 'dihedral']),
-        'img_size': tune.grid_search([28]),
+        'img_size': tune.grid_search([32]),
         'target_params_count': tune.sample_from(spec_to_target_params),
         'data_augmentation': tune.sample_from(data_augmentation),
         'fc_sizes': tune.sample_from(fc_sizes),
@@ -241,7 +223,7 @@ def search_filters():
 
 def search_group_size():
     scaling_config = ScalingConfig(
-        num_workers=1, use_gpu=True, resources_per_worker={'CPU': 3, 'GPU': 1}
+        num_workers=1, use_gpu=True, resources_per_worker={'CPU': 10, 'GPU': 1}
     )
 
     run_config = RunConfig(
@@ -263,14 +245,10 @@ def search_group_size():
     def spec_to_target_params(spec):
         target_params = {
             'cyclic': {
-                'MNIST': 40000,
-                'EMNIST': 40000,
-                'CIFAR10': 500000,
+                'CIFAR10': 1000000,
             },
             'dihedral': {
-                'MNIST': 140000,
-                'EMNIST': 40000,
-                'CIFAR10': 500000,
+                'CIFAR10': 1000000,
             },
         }
         config = spec['train_loop_config']
@@ -283,27 +261,27 @@ def search_group_size():
 
     def n_filters(spec):
         n_filters_map = {
-            'cyclic': {'MNIST': 24},
-            'dihedral': {'MNIST': 4},
+            'cyclic': {'CIFAR': 24},
+            'dihedral': {'CIFAR': 4},
         }
         config = spec['train_loop_config']
         return n_filters_map[config['group']][config['dataset_name']]
 
     def fc_sizes(spec):
         fc_sizes_map = {
-            'MNIST': [64, 64, 10],
+            'CIFAR': [64, 64, 10],
         }
         config = spec['train_loop_config']
         return fc_sizes_map[config['dataset_name']]
 
     def n_input_channels(spec):
-        n_input_channels_map = {'MNIST': 1, 'EMNIST': 1, 'CIFAR10': 3}
+        n_input_channels_map = {'CIFAR10': 3}
         config = spec['train_loop_config']
         return n_input_channels_map[config['dataset_name']]
 
     search_space = {
         'pooling': tune.grid_search(['bsp', 'tc', 'max', 'avg']),
-        'dataset_name': tune.grid_search(['MNIST']),
+        'dataset_name': tune.grid_search(['CIFAR10']),
         'group': tune.grid_search(['cyclic', 'dihedral']),
         'img_size': tune.grid_search([28]),
         'target_params_count': tune.sample_from(spec_to_target_params),
@@ -338,7 +316,7 @@ def search_group_size():
 if __name__ == '__main__':
     results = search_pooling()
     results.get_dataframe().to_csv('ray_results_seach_pooling.csv')
-    results = search_filters()
-    results.get_dataframe().to_csv('ray_results_filters.csv')
-    results = search_group_size()
-    results.get_dataframe().to_csv('ray_results_group_size.csv')
+    # results = search_filters()
+    # results.get_dataframe().to_csv('ray_results_filters.csv')
+    # results = search_group_size()
+    # results.get_dataframe().to_csv('ray_results_group_size.csv')
